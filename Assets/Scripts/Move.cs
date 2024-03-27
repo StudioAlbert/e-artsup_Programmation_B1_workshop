fdd04179f3af19fb19c841a067a6df23175d9f34
StudioAlbert <account@studio-albert.com>
2024-03-27 15:00:54 +0100

    brand new repo

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField] private float maxSpeed = 10;
+    [SerializeField] private float rotationSpeed = 25;
+    
+    private Transform localTransform;
+
+    private float rotateAxis;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        localTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        /*
+        localTransform.Rotate(Vector3.up, Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime);
+        localTransform.Translate(Input.GetAxis("Vertical") * Vector3.forward * maxSpeed * Time.deltaTime);
+        */
+        
+        localTransform.Rotate(Vector3.up, rotateAxis * rotationSpeed * Time.deltaTime);
+        
+        
+    }
+
+    public void HandleRotate(InputAction.CallbackContext inputContext)
+    {
+        rotateAxis = inputContext.ReadValue<float>();
+        Debug.Log("Rotate  = " + rotateAxis);
+    }
+
+}
